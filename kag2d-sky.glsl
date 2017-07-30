c839d2401af5c67f2cfff80c835811c7dd819cca
Asu <AsuMagic@users.noreply.github.com>
2017-07-30 22:32:32 +0200

    Create kag2d-sky.glsl

---
+++ kag2d-sky.glsl
@@ -0,0 +1,48 @@
+float rand(vec2 seed)
+{
+    return fract(sin(dot(seed, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
+vec3 dither(vec3 color, float noise)
+{
+    return color + mix(-0.5 / 255., 0.5 / 255., noise);
+}
+
+float luma(vec3 color)
+{
+    return dot(color, vec3(0.299, 0.587, 0.114));
+}
+
+vec3 getSkyColorAt(vec2 texCoord)
+{
+    return vec3(texture(iChannel0, texCoord / iChannelResolution[0].xy));
+}
+
+vec3 getSkyColor(float daytime, vec2 position)
+{
+    return mix(getSkyColorAt(vec2(daytime, 1)),
+               getSkyColorAt(vec2(daytime, 0)),
+              distance(position, vec2(0.5, 0.0)) * 1.5 * position.y);
+}
+
+vec3 getStarColor(vec2 position, vec3 noise)
+{
+    if (rand(position * noise.r) > 0.998) // HACK: * noise.r to work around rand()'s precision
+    {
+        vec3 randomcoloroff = vec3(noise.r / 9., noise.g / 6., noise.b / 9.);
+        return rand(iTime * position) * 0.15 + vec3(0.7) - randomcoloroff;
+    }
+
+    return vec3(0);
+}
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord)
+{
+    vec2 position = fragCoord.xy / iResolution.xy;
+    vec3 noise = vec3(texture(iChannel1, fragCoord / iChannelResolution[1].xy));
+
+    vec3 skycolor = getSkyColor(iTime * 8., position);
+    vec3 starcolor = getStarColor(position, noise);
+
+    fragColor = vec4(max(starcolor - luma(skycolor), dither(skycolor, noise.b)), 0);
+}
